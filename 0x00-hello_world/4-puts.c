fd560872aec2ee42e794a8dfcc3c4c2eb3ab5dba
zouhairjilal <zedprankster@gmail.com>
2023-06-15 14:57:04 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  * Return: 0 if exited properly, non-zero otherwise
  */
 
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
