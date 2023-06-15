a04f57864d98d9666d9662a1af2675828b0ad287
zouhairjilal <zedprankster@gmail.com>
2023-06-15 11:54:00 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
-/* a script that print "Programming is like building a multilingual puzzle */
 #include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
