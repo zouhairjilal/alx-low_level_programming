1eccb246b97519414613aca933ebb9fe68590030
zouhairjilal <zedprankster@gmail.com>
2023-06-15 15:28:05 -0700

    Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-/*
- * script that print 'with proper grammar, but the outcome is a piece of art,'
- */
-
 #include <stdio.h>
-int main(void) /*hello*/
+
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
 {
-/* print this cool thing */
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
