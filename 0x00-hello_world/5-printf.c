4c8553aea6686993bbcf0c678eee6aecb46d628b
zouhairjilal <zedprankster@gmail.com>
2023-06-15 15:15:42 -0700

    Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 /*
- * script that print with proper grammar, but the outcome is a piece of art,
+ * script that print 'with proper grammar, but the outcome is a piece of art,'
  */
+
 #include <stdio.h>
 
 int main(void)
