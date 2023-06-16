204a000f1d9962117584e07c7545c72a520d65ed
zouhairjilal <zedprankster@gmail.com>
2023-06-16 16:46:24 -0700

    Update

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+
+	return (0);
+}
