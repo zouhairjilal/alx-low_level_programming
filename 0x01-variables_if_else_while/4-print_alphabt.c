a1bb1f13caf31b16eb8095dbe4c58c80acb109cf
zouhairjilal <zedprankster@gmail.com>
2023-06-17 07:45:46 -0700

    Update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a script that prints alphabits in lowercase 
+ * except q and e ony using putchar,
+ * Return: if return is 0 is a good sign :) 
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101 || i == 113)
+			i++;
+		else
+			putchar(i);
+	}
+	putchar('\n');
+}
