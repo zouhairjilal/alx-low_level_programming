2b7b9458ffb41884984468db94143c42d6b49f4c
zouhairjilal <zedprankster@gmail.com>
2023-06-17 09:07:49 -0700

    Update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
+ * i can use only putchar and only twice.
+ * Return: if return is 0 tha's a good sign.
+ */
+int main(void)
+{
+	int num;
+	int alphbt;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+
+	for (alphbt = 65; alphbt <= 70; alphbt++)
+		putchar(alphbt);
+	
+	putchar('\n');
+
+	return (0);
+}
