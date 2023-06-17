ba21d21cf0116bbdf46832131726f66f33d28c16
zouhairjilal <zedprankster@gmail.com>
2023-06-17 06:06:59 -0700

    Update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-/*
- *
+/**
+ * main - a script that print alphabits upper case and lower case,
+ * return: 0 is a good sign :)
  */
 
 int main(void)
 {
-	for (int i=97; i < 123; i++)
+	for (int i = 97; i < 123; i++)
 		printf("%c", i);
 
-	for (int i=65; i < 90; i++)
+	for (int i = 65; i < 90; i++)
 		printf("%c", i);
+
+	return (0);
 }
 
