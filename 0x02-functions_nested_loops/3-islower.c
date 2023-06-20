d954d0d83a051c2dec9a9a92fd810554ea7f15cd
zouhairjilal <zedprankster@gmail.com>
2023-06-20 06:24:04 -0700

    Update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - this function return 1 if c is lower,
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	int lower;
+	
+	for (lower = 'a'; lower < 'z'; lower++){
+		if ( c == lower;)
+			return (1);
+		else
+			return (0);
+
+}
