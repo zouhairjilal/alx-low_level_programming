20b5a102282c7cd6b770cf18c1cb260360942460
zouhairjilal <zedprankster@gmail.com>
2023-06-20 04:43:24 -0700

    Update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints _putchar, followed by a new line.
+ * Return: if return is 0 that's a good sign.
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return(0);
+}
