292669f942dc574f551f145ee2fc7e0d4922d7f0
zouhairjilal <zedprankster@gmail.com>
2023-06-20 06:58:00 -0700

    Update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,19 +9,18 @@ int _islower(int c)
 {
     int lower;
     int upper;
+
     for (lower = 'a'; lower < 'z'; lower++)
     {
         if ( c == lower)
-        {
            return (1);
-        }
+        
     }
  
     for (upper = 'A'; upper < 'Z'; upper++)
     {
         if ( c == upper)
-        {
             return (0);
-        }
+        
     }
-
+}
