5a849daa3d95000be717d9ce37f6fcac0b673af4
zouhairjilal <zedprankster@gmail.com>
2023-06-20 16:57:43 -0700

    Update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result.
+ * Return: reterning the value.
+ */
+
+int add(int a, int b)
+{
+	return (a+b);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hours = 00;
+	while (hours < 24)
+	{
+		_putchar(hours);
+		hours++;
+	}
+}
