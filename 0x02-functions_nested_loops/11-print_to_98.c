591bd3a0a6965eb0d06d03f21b6455d433c64c92
zouhairjilal <zedprankster@gmail.com>
2023-06-20 17:16:22 -0700

    Update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * 
+ *
+ */
+
+void print_to_98(int n)
+{
+    int n = 0;
+    while (n <= 98)
+    {
+        if (n == 98)
+        {
+            printf("%d", n);
+        }
+        else
+        {
+            printf("%d, ", n);
+            
+        }
+        n++;
+    }
+}
